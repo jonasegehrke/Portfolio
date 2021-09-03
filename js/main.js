451094fb88f3415a759f5dc29ac5b4ed3652d748
Jonas Gehrke <jonasegherke@gmail.com>
2021-09-03 08:51:48 +0200

    Media queuries

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openNav() {
+    var links = document.getElementById("nav_mobile_links");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+
+  }
+
